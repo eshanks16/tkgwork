ff52d3fad6b5370b69d74deae451a33957cf6d31
Eric Shanks <eshanks@eshanks-a01.vmware.com>
2020-09-28 10:56:57 -0500

    Updating script to write multiple file entries

--- dexbootstrap/script.sh
+++ dexbootstrap/script.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 repo=$1
-file=$2
-branch=$3
-parameter=$4 #YQ Value
+branch=$2
+cluster_name=$3
+lb_cert=$4
+filename=$cluster_name-tkg-mgmt.yaml
 
 #Pull down the git repository with the Values file to be updated.
 GIT_SSH_COMMAND='ssh -i /etc/ssh-key/vmw-mac' git clone $repo
@@ -21,9 +22,29 @@ done
 echo "The load balancer address is: $lbaddress"
 
 #Write the load balancer address into the values file
-yq w -i $file $parameter $lbaddress
+#yq w -i $file $parameter $lbaddress
+
+####Additions
+if [ $(ls $cluster_name-tkg-mgmt.yaml) -eq 0 ]
+then
+    #File already exists - Update File
+    echo "File exists - updating in place"
+    yq w -i $filename DEX_LB_HOSTNAME $lbaddress
+    yq w -i $filename CNAME $cluster_name-tkgdex.aws.imovetv.com
+    yq w -i $filename LB_CERT $lb_cert
+else
+    #File does not exist - Create File
+    echo "File does NOT exist"
+    cat > $filename
+    yq w -i $filename DEX_LB_HOSTNAME $lbaddress
+    yq w -i $filename CNAME $cluster_name-tkgdex.aws.imovetv.com
+    yq w -i $filename LB_CERT $lb_cert
+fi
+
+#############
+
 
 #Push the changes back to git
-git add $file
+git add $filename
 git commit -m "Updating Dex LoadBalancer Value"
 GIT_SSH_COMMAND='ssh -i /etc/ssh-key/vmw-mac' git push
